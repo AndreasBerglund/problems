d961a8e6785d4b89a030bb57f0a0d3ff2c6a306e
andreasmonkey <andreasfabrinberglund@gmail.com>
2022-10-31 22:16:30 +0100

    fix max bug

--- max.js
+++ max.js
@@ -4,14 +4,17 @@
  * @returns the highest number
  */
 function max(arr) {
-  // Base case, arr length 2 return the highest number of the two
-  if (arr.length === 0) return 0;
-  if (arr.length === 2) return arr[0] > arr[1] ? arr[0] : arr[1];
-  arr.shift();
-  const sub_max = max(arr);
-  return arr[0] > sub_max ? arr[0] : sub_max;
+  // Base case, arr length 1 return the number
+  if (arr.length < 2) {
+    return arr.length === 0 ? 0 : arr[0];
+  }
+  const [first, ...next_arr] = arr;
+  const sub_max = max(next_arr);
+  return first > sub_max ? first : sub_max;
 }
 
 console.log(max([1, 2, 3])); //3
 console.log(max([])); //0
 console.log(max([1, 6, 2, 100])); //100
+console.log(max([1000, 6, 2, 100])); //1000
+console.log(max([1000, 6, -22134423, 54604, 100])); //54604
