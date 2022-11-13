5a156f9dede6ced1b1d3c62e3ec86482d41af904
andreasmonkey <andreasfabrinberglund@gmail.com>
2022-11-13 21:13:24 +0100

    hashmap implementation

---
+++ algorithms/hashmap.js
@@ -0,0 +1,50 @@
+/**
+ * The simplest and stupidest way to implement a pseudo hashmap of length 5
+ */
+const hashmap = {
+  map: [[], [], [], [], []],
+  find: function (key) {
+    const index = this.hash(key);
+    if (this.map[index].length > 0) {
+      for (let i = 0; i < this.map[index].length; i++) {
+        if (this.map[index][i][0] === key) return this.map[index][i][1];
+      }
+    }
+  },
+  insert: function (key, value) {
+    const index = this.hash(key);
+    this.map[index].push([key, value]);
+  },
+  /**
+   * This is the function that assigns a slot in the map / array
+   * @param {string} key
+   * @returns {number}
+   */
+  hash: function (key) {
+    // Map every letter to a prime number, the assigned slot is sum of prime number modulo length of array
+    const az = "abcdefghijklmnopqrstuvwxyz".split(""); // this should not be done every time, but oh well
+    const primes = [
+      2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
+      71, 73, 79, 83, 89, 97, 101,
+    ];
+    // Sum letter prime numbers
+    let sum = 0;
+    for (const c in key) {
+      const index = az.indexOf(key[c]);
+      sum += primes[index];
+    }
+    return sum % 5;
+  },
+};
+
+hashmap.insert("pear", 5);
+hashmap.insert("apple", 10);
+hashmap.insert("banana", 10);
+hashmap.insert("pineapple", 10);
+hashmap.insert("lemon", 10);
+
+const lemon_price = hashmap.find("lemon"); //10
+const cucumber_price = hashmap.find("cucumber"); //undefined
+
+console.log(lemon_price);
+console.log(cucumber_price);
